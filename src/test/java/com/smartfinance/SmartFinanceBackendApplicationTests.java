ff3714ec7c0e25cf005061672fabc7ccb7df5332
TuanAnhVu81 <tuananhvu1123@gmail.com>
2026-03-09 14:05:19 +0700

    Initial commit

---
+++ src/test/java/com/smartfinance/SmartFinanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
